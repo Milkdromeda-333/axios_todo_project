a4f80a63315bceeba176eff6ba088c7bc15bf4c6
Anjanique Mackey <anjaniquemackey@gmail.com>
2022-07-01 16:29:48 -0700

    - update

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-const axios = require("axios");
+const axios = require('axios');
 
+const form = document["myForm"];
+const ul = document.getElementsByTagName("ul")[0];
+
+let toDoList = axios.get("https://api.vschool.io/anjanique/todo")
+    .then(response => console.log(response.data));
