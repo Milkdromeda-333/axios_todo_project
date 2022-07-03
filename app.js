47cccbef8c11f8bdd836ece7b58330c5f20b90d2
Anjanique Mackey <anjaniquemackey@gmail.com>
2022-07-03 16:59:25 -0700

    add items feature  created

--- app.js
+++ app.js
@@ -1,7 +1,44 @@
-const axios = require('axios');
-
+let url = "https://api.vschool.io/andromeda/todo";
 const form = document["myForm"];
 const ul = document.getElementsByTagName("ul")[0];
+const submitBtn = document.getElementsByTagName("button")[0];
+let toDo = document.getElementById("listItem").value;
+let toDoDescription = document.getElementById("listDescription").value;
 
-let toDoList = axios.get("https://api.vschool.io/anjanique/todo")
-    .then(response => console.log(response.data));
+let getData = axios.get(url)
+    .then(response => addCurrentList(response.data))
+    .catch(err => console.log(err));
+
+// this updates the list to have all of the current items
+const addCurrentList = (data) => {
+    // this allows there to be a notification that there is no data yet if ther is none. 
+    if (!data) {
+        return;
+    }
+
+    ul.innerHTML = "";
+    for (let listData of data) {
+        let listItem = document.createElement("li");
+        listItem.innerHTML = listData.title + ": " + listData.description;
+        ul.appendChild(listItem);
+    }
+};
+
+// This adds the item after being submitted
+form.addEventListener("submit", e => {
+    e.preventDefault(); // prevent refresh of the page
+    let toDoTitle = document.getElementById("listItem").value;
+    let toDoDescription = document.getElementById("listDescription").value;
+
+    const listItem = {
+        title: toDoTitle,
+        description: toDoDescription
+    };
+
+    axios.post(url, listItem);
+
+    document.getElementById("listItem").value = "";
+    document.getElementById("listDescription").value = "";
+
+    getData();
+});
