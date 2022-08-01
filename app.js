02a295cb434b83832229763f76587f07c70b6f9e
Anjanique Mackey <anjaniquemackey@gmail.com>
2022-08-01 07:17:43 -0700

    completed project(!!!!)

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-let url = "https://api.vschool.io/andromeda/todo";
+let url = "https://api.vschool.io/andromeda/todo/";
 const form = document["myForm"];
 const list = document.getElementsByClassName("list")[0];
-const submitBtn = document.getElementsByTagName("button")[0];
-let toDo = document.getElementById("listItem").value;
-let toDoDescription = document.getElementById("listDescription").value;
 
 // this adds the current list to the screen on loading
 let getData = axios.get(url)
@@ -12,65 +9,68 @@ let getData = axios.get(url)
 
 
 // this updates the list to have all of the current items
-
 const addCurrentList = (data) => {
-    // this allows there to be a notification that there is no data yet if there is none. 
+
+    // this allows there to be a notification that there is no data yet if there is none.
     if (!data) {
         return;
     }
 
     list.innerHTML = "";
     for (let listData of data) {
-        let listItem = document.createElement("li");
-        let checkbox = document.createElement("input");
-        checkbox.setAttribute("type", "checkbox");
-        listItem.prepend(checkbox);
-
-
-        let p = document.createElement("p");
-        p.textContent = listData.title;
-        listItem.append(p);
-
-        if (listData.completed === true) {
-            p.style.textDecoration = "line-through";
-            p.style.textDecorationColor = "rgb(246, 94, 190)";
-        }
-
-        let imgUrl = listData.imgUrl;
-        if (imgUrl !== undefined) {
-            const img = document.createElement("img");
-            img.src = imgUrl;
-            listItem.appendChild(img);
-        }
-
-        let result = `${listData.title}`;
-        if (listData.description) {
-            result.concat(": ", listData.description);
-        }
-        if (listData.price !== null) {
-            result.concat(" ", listData.price);
-        }
-
-
-        list.appendChild(listItem);
-        // listItem.appendChild(text);
+        addItems(listData);
     }
 };
 
-const addNewItem = data => {
+const addItems = data => {
+
     let listItem = document.createElement("li");
     let checkbox = document.createElement("input");
     checkbox.setAttribute("type", "checkbox");
     listItem.prepend(checkbox);
 
 
+
+    let result = `${data.title}`;
+    if (data.description) {
+        result = result.concat(": ", data.description);
+    }
+    if (data.price) {
+        result = result.concat(" ", data.price);
+    }
     let p = document.createElement("p");
-    p.textContent = data.title;
+    p.append(result);
     listItem.append(p);
 
-    if (data.completed === true) {
+    let itemUrl = url + data._id;
+
+    checkbox.addEventListener("change", (e) => {
+        if (e.target.checked) {
+            axios.put(itemUrl, {
+                completed: true
+            })
+                .catch(err => console.log(err));
+
+            p.style.textDecoration = "line-through";
+            p.style.textDecorationColor = "rgb(246, 94, 190)";
+            checkbox.checked = true;
+        }
+    });
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "x";
+    deleteBtn.setAttribute("style", "margin-left: 10px;");
+    listItem.append(deleteBtn);
+
+    deleteBtn.addEventListener("click", () => {
+        axios.delete(itemUrl).then(res => alert(res.data.msg)).then(list.removeChild(listItem)).catch((err) => alert(err));
+        console.log(itemUrl);
+    });
+
+    if (data.completed) {
         p.style.textDecoration = "line-through";
         p.style.textDecorationColor = "rgb(246, 94, 190)";
+        checkbox.checked = true;
     }
 
     let imgUrl = data.imgUrl;
@@ -80,39 +80,7 @@ const addNewItem = data => {
         listItem.appendChild(img);
     }
 
-    let result = `${data.title}`;
-    if (data.description) {
-        result.concat(": ", data.description);
-    }
-    if (data.price !== null) {
-        result.concat(" ", data.price);
-    }
-
-
     list.appendChild(listItem);
-    //
-    // let listItem = document.createElement("input");
-    // listItem.setAttribute("type", "checkbox");
-
-    // listItem.textContent = `${data.title}: ${data.description} ${data.price}`;
-
-    // if (data.completed === true) {
-    //     listItem.style.textDecoration = "line-through";
-    //     listItem.style.textDecorationColor = "rgb(246, 94, 190)";
-    // }
-
-    // let imgUrl = data.imgUrl;
-    // if (imgUrl !== undefined) {
-    //     const img = document.createElement("img");
-    //     img.src = imgUrl;
-    //     listItem.appendChild(img);
-    // }
-
-    // list.appendChild(listItem);
-
-    listItem.addEventListener("click", () => {
-
-    });
 };
 
 // This adds the item after being submitted
@@ -131,7 +99,7 @@ form.addEventListener("submit", e => {
     };
 
     axios.post(url, listItem)
-        .then(response => addNewItem(response.data))
+        .then(response => addItems(response.data))
         .catch(err => console.log(err));
 
 
